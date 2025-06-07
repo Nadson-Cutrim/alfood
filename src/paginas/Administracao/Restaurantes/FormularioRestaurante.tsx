ca58a1e79eecec634812bd034abd20cc4db69c39
Nadson-Cutrim <nadsoncutrimjunior@aluno.uema.br>
2025-06-07 11:04:14 -0300

    ajustando layout do form

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,61 +1,73 @@
-import { Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import IRestaurante from "../../../interfaces/IRestaurante";
 
 const FormularioRestaurante = () => {
-    const parametros = useParams();
+  const parametros = useParams();
 
-    useEffect(() => {
-        if (parametros.id) {
-            axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
-            .then(resposta => {
-                setNomeRestaurante(resposta.data.nome);
-            })
-            .catch(erro => {
-                console.log(erro);
-            });
+  useEffect(() => {
+    if (parametros.id) {
+      axios
+        .get<IRestaurante>(
+          `http://localhost:8000/api/v2/restaurantes/${parametros.id}/`
+        )
+        .then((resposta) => {
+          setNomeRestaurante(resposta.data.nome);
+        })
+        .catch((erro) => {
+          console.log(erro);
+        });
+    }
+  }, [parametros]);
 
-        }
-    },[parametros]);
+  const [nomeRestaurante, setNomeRestaurante] = useState("");
 
+  const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault();
+    if (parametros.id) {
+      axios
+        .put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+          nome: nomeRestaurante,
+        })
 
-    const [nomeRestaurante, setNomeRestaurante] = useState("");
+        .then(() => {
+          alert("Restaurante atualizado com sucesso!");
+        });
+    } else {
+      axios
+        .post("http://localhost:8000/api/v2/restaurantes/", {
+          nome: nomeRestaurante,
+        })
 
+        .then(() => {
+          alert("Restaurante cadastrado com sucesso!");
+        });
+    }
+  };
 
-    const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
-        evento.preventDefault();
-        if (parametros.id) {
-          axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`,
-                {nome: nomeRestaurante})
-            
-                .then(() =>{
-                alert("Restaurante atualizado com sucesso!");
-            })  
-        } else {
-                axios.post("http://localhost:8000/api/v2/restaurantes/",
-                {nome: nomeRestaurante})
-            
-                .then(() =>{
-                alert("Restaurante cadastrado com sucesso!");
-            })
-        }
-}
-
-return (
-    <form onSubmit={aoSubmeterForm}>
-        <TextField 
-        value={nomeRestaurante} 
-        onChange={
-            evento => setNomeRestaurante(evento.target.value)} 
-            id="standard-basic" label="Nome do Restaurante" variant="standard"/>
-        <Button type="submit" variant="outlined">
-            Salvar
+  return (
+    <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <Box component="form" onSubmit={aoSubmeterForm}>
+        <Typography component="h1" variant="h6">
+          Formulário de Restaurantes
+        </Typography>
+        <TextField
+          value={nomeRestaurante}
+          onChange={(evento) => setNomeRestaurante(evento.target.value)}
+          id="standard-basic"
+          label="Nome do Restaurante"
+          variant="standard"
+          fullWidth
+          required
+        />
+        <Button type="submit" variant="outlined" fullWidth sx={{ marginTop: 2 }}>
+          Salvar
         </Button>
-    </form>
-)
-
-}
+      </Box>
+    </Box>
+  );
+};
 
 export default FormularioRestaurante;
